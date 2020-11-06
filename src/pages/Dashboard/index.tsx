4cb5c5339ebf47ae81c5b074196a63a41cce5668
alissonsleal <alissonsleal@hotmail.com>
2020-11-06 19:34:40 -0300

    api is connected and working with the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,63 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent) {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore Github repositories</Title>
-      <Form>
-        <input type="text" placeholder="Type the repository name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Type the repository name"
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://images.unsplash.com/photo-1604596610075-4b2b06b5eb93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=294&q=80"
-            alt="person name"
-          />
-          <div>
-            <strong>personName/repoName</strong>
-            <p>repoDescription</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://images.unsplash.com/photo-1604596610075-4b2b06b5eb93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=294&q=80"
-            alt="person name"
-          />
-          <div>
-            <strong>personName/repoName</strong>
-            <p>repoDescription</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://images.unsplash.com/photo-1604596610075-4b2b06b5eb93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=294&q=80"
-            alt="person name"
-          />
-          <div>
-            <strong>personName/repoName</strong>
-            <p>repoDescription</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
